e8ba652b12d5cad3122f16fb88539bfa14d74923
critz <john.critz@gmail.com>
2012-05-17 15:22:45 -0500

    tweak example web server output

--- examples/web_server.rb
+++ examples/web_server.rb
@@ -3,5 +3,5 @@ require './lib/fb_burrito.rb'
 
 get '/facebook' do
   user = FbBurrito.find_or_create_user!(:auth_code => params[:code])
-  "user: #{user.inspect}".strip
+  "access_token: #{user.fb_token}".strip
 end
